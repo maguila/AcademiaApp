683c05797b89d0b302d69f3178f1d5ecb64d4650
miguel <m.aguila@solcloud.cl>
2016-07-21 17:24:42 -0400

    Commit para clase 21/07/2016
    SQLite, con un handler, operaciones CRUD

--- app/src/main/java/cl/academia/academiaapp/HttpActivity.java
+++ app/src/main/java/cl/academia/academiaapp/HttpActivity.java
@@ -1,23 +1,37 @@
 package cl.academia.academiaapp;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import cl.academia.academiaapp.sqlLite.DataBaseHelper;
 import cl.academia.academiaapp.sqlLite.UsuarioPojo;
@@ -44,7 +58,31 @@ public class HttpActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        //CREO EL LIST VIEW Y AGREGO VALORES AL ADAPTADOR DESDE UN JSON REMOTO
+        if(isConexionActiva())
+            initListViewDesdeJson();
+        else{
+            Toast.makeText(this, "No hay conexion, se debe traer datos de la base de datos ...", Toast.LENGTH_SHORT).show();
 
+            ListView listaUsuarios = (ListView) findViewById(R.id.lista_usuarios);
+            ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+
+            DataBaseHelper dbHelper = new DataBaseHelper(this, false);
+            for(UsuarioPojo o : dbHelper.getAllUsuarios()){
+                Log.d("LECTURA DE SQLite ... ", "NOMBRE : " + o.getNombre() + ",  APELLIDO : " + o.getApellido());
+                adapter.add(o.getNombre() + " " + o.getApellido() );
+            }
+
+            TextView text = (TextView) findViewById(R.id.cantidad_usuarios_text);
+            text.setText(dbHelper.countUsuarios()+"");
+            listaUsuarios.setAdapter(adapter);
+
+        }
+
+
+    }
+
+    public void initListViewDesdeJson(){
         //INSTANCIAMOS NUESTRO LIST VIEW
         ListView listaUsuarios = (ListView) findViewById(R.id.lista_usuarios);
         ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
@@ -95,40 +133,128 @@ public class HttpActivity extends AppCompatActivity {
 
         //ASOCIAMOS EL LIST VIEW AL ADAPTADOR
         listaUsuarios.setAdapter(adapter);
-
     }
 
 
-
-    public void insertDB(String nombre, String apellido){
-        DataBaseHelper dbHelper = new DataBaseHelper(this);
-        Log.d("INSERT ... ", "insertando en base de datos el usuario " + nombre);
-
-        dbHelper.addUsuario(new UsuarioPojo(null, nombre, apellido));
-    }
 
     public void leerBD(View view){
-        DataBaseHelper dbHelper = new DataBaseHelper(this);
+        DataBaseHelper dbHelper = new DataBaseHelper(this, false);
         for(UsuarioPojo o : dbHelper.getAllUsuarios()){
-            System.out.println(o.getNombre());
+            Log.d("LECTURA DE SQLite ... ", "NOMBRE : " + o.getNombre() + ",  APELLIDO : " + o.getApellido());
         }
+
+        TextView text = (TextView) findViewById(R.id.cantidad_usuarios_text);
+        text.setText(dbHelper.countUsuarios()+"");
     }
 
 
     public void sincronizar(View view){
-        DataBaseHelper dbHelper = new DataBaseHelper(this);
 
-        try{
-            for(int i = 0; i<jsonArray.length(); i++){
-                JSONObject o    = jsonArray.getJSONObject(i);
-                String nombre   = o.getString("nombre");
-                String apellido = o.getString("apellido");
-                insertDB(nombre, apellido);
+        if(isConexionActiva()){
+
+            DataBaseHelper dbHelper = new DataBaseHelper(this, false);
+            dbHelper.deleteAllUsuarios();
+
+            try{
+                for(int i = 0; i<jsonArray.length(); i++){
+                    JSONObject o    = jsonArray.getJSONObject(i);
+                    String nombre   = o.getString("nombre");
+                    String apellido = o.getString("apellido");
+                    insertDB(nombre, apellido);
+                }
+            }catch(JSONException e){
+                e.printStackTrace();
             }
-        }catch(JSONException e){
+
+
+            TextView text = (TextView) findViewById(R.id.cantidad_usuarios_text);
+            text.setText(dbHelper.countUsuarios()+"");
+        }else{
+            Toast.makeText(this, "No hay conexion, no es posible sincronizar datos ...", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public void insertDB(String nombre, String apellido){
+        DataBaseHelper dbHelper = new DataBaseHelper(this, false);
+        Log.d("INSERT ... ", "insertando en base de datos el usuario " + nombre);
+        dbHelper.addUsuario(new UsuarioPojo(null, nombre, apellido));
+    }
+
+    public void crearUsuario(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Datos nuevo usuario");
+
+        //ASOCIAR UN LAYOUT REMOTO
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View viewRemoto = inflater.inflate(R.layout.crear_usuario,null);
+        builder.setView(viewRemoto);
+
+
+
+        builder.setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                EditText editNombre = (EditText) viewRemoto.findViewById(R.id.editText);
+                EditText editApellido = (EditText) viewRemoto.findViewById(R.id.editText2);
+
+                postRequestURL("http://server2.solcloud.cl/academia/json.php", editNombre.getText().toString(), editApellido.getText().toString());
+
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void postRequestURL(String url, String nombre, String apellido){
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("accion", "crear");
+        params.put("nombre", nombre);
+        params.put("apellido", apellido);
+
+
+        String parametrosURL = "?accion=crear&nombre="+nombre+"&apellido="+apellido;
+
+
+        HttpURLConnection conn = null;
+        try{
+            conn = (HttpURLConnection) (new URL(url+parametrosURL)).openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.connect();
+            InputStream is = conn.getInputStream();
+
+            BufferedReader buff = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            String line = "-";
+
+            while( (line = buff.readLine()) !=null ){
+                sb.append(line).append(" \n");
+            }
+
+            System.out.println("SALIDA .. " + sb.toString());
+
+        }catch(IOException e){
             e.printStackTrace();
         }
 
+    }
+
+
+
+
+    private boolean isConexionActiva() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
 }
--- app/src/main/java/cl/academia/academiaapp/sqlLite/DataBaseHelper.java
+++ app/src/main/java/cl/academia/academiaapp/sqlLite/DataBaseHelper.java
@@ -27,7 +27,7 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 
 
 
-    public DataBaseHelper(Context context){
+    public DataBaseHelper(Context context, boolean isSincronizar){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
@@ -101,7 +101,7 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 
     public List<UsuarioPojo> getAllUsuarios(){
         List<UsuarioPojo> usuarioList = new ArrayList<>();
-        String sql = "SELECT * FROM " + KEY_NOMBRE_TABLA;
+        String sql = "SELECT "+KEY_USUARIO+"  , "+KEY_NOMBRE+" , "+KEY_APELLIDO+" FROM " + KEY_NOMBRE_TABLA;
 
         SQLiteDatabase db = this.getWritableDatabase();
 
@@ -115,12 +115,28 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 
         if(cursor!=null && cursor.moveToFirst()){
             do{
-               usuarioList.add(new UsuarioPojo(cursor.getString(1), cursor.getString(2), cursor.getString(3) ));
+               usuarioList.add(new UsuarioPojo(cursor.getString(0), cursor.getString(1), cursor.getString(2) ));
             }while(cursor.moveToNext());
 
         }
         db.close();
         return usuarioList;
+    }
 
+    public int countUsuarios(){
+        int retorno = 0;
+        String sql = "SELECT * FROM " + KEY_NOMBRE_TABLA;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+        retorno = cursor.getCount();
+        cursor.close();
+        db.close();
+        return retorno;
+    }
+
+    public void deleteAllUsuarios(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + KEY_NOMBRE_TABLA);
+        db.close();
     }
 }
